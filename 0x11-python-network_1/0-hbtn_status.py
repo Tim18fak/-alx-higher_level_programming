1b4a8dd8d123505fb7c488a0a5b273384d80e071
Tim18fak <Tim18fak@github.com>
2023-07-02 15:14:58 -0700

    pytthon networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Fetches https://alx-intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
+
 if __name__ == "__main__":
-    link = "https:/alx-intranet.btn.io/status"
-    request = urllib.request.Request(link)
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: %s" % type(body))
-        print("\t- content: " + str(body))
-        print("\t- utf8 content: " + str(body.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
