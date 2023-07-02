f4751aec6207f90700c7f5bc9f8f42374897180d
Tim18fak <Tim18fak@github.com>
2023-07-02 16:54:30 -0700

    python networkin

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@ read_file function
 """
 
 
-def read_file(fil=""):
+def read_file(filename=""):
     """""reads a text file(UTF8) and prints it to stdout"""
-    with open(fil, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
