9f446576a7e3c13a1ad0f0569e03b2d862e8773b
Tim18fak <Tim18fak@github.com>
2023-07-02 15:18:53 -0700

    pytthon networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}" + str(type(body)))
+        print("\t- content: {}" + str(body))
+        print("\t- utf8 content: {}" + str(body.decode("utf-8")))
