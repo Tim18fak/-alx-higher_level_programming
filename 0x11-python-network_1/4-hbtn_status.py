d3fb3bd766567f032cbafab5906c646b1e861941
Tim18fak <Tim18fak@github.com>
2023-07-02 15:46:30 -0700

    pytthon networking

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""retrieve  https://alx-intranet.hbtn.io/status using requests modules"""
 import requests
 if __name__ == "__main__":
     fetch = requests.get("https://alx-intranet.hbtn.io/status")
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+Usage: python3 6-post_email.py <URL> <email>
+  - show the body of the response.
+"""
+import requests
+import sys
+
+
+
+if __name__ == "__main__":
+    link = sys.argv[1]
+    email = {"email": sys.argv[2]}
+
+    r = requests.post(link, data=email)
+    print(r.text)
